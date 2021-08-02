d83d24b0ae20dadbdd312d927db749e73a33650e
thomaz jefferson <thomaz.jefferson01@protonmail.com>
2021-08-02 15:17:07 -0300

    add

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log (numbers)
+
+for (var contar = 0; contar < numbers.length; contar += 1) {
+    let soma = numbers[contar]
+    
+    numbers ++
+
+}
