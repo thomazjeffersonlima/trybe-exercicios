5246d533bdd1670c5c23b9701259068fce881a4a
thomaz jefferson <thomaz.jefferson01@protonmail.com>
2021-08-02 15:33:08 -0300

    adicionado um array bestintheworld ao objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio.js
@@ -8,4 +8,7 @@ let player = {
     }
 }
 
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+
 console.log ('A jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + 'anos de idade.')
+console.log(player)
