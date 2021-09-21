b2d852f515471b4cae142442f93377cec8b646ba
thomazj <thomaz.jefferson01@protonmail.com>
2021-09-21 18:38:59 -0300

    correção

---
+++ fundamentos/bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exercicio1.js
@@ -0,0 +1,11 @@
+// apiScript.js
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+  const myObject = {
+    
+  }
+};
+
+window.onload = () => fetchJoke();
