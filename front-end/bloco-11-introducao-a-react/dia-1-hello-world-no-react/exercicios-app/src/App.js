2582517c64fdc8e1ba6b4fadd7600b2c6b63bfe4
thomazj <thomaz.jefferson01@protonmail.com>
2021-10-04 21:19:41 -0300

    adicionar blocos como paragrafo

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercicios-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/exercicios-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Lista from './lista'
 import Header from './Header'
+import Content from './Content'
 
 function App() {
   return ( 
     <div>
       <Lista />
       <Header />
+      <Content />
     </div>
     );
 }
